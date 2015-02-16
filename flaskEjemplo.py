dba5ef72f9aaf9a8b544dc41cffdd4e1f7dfbb04
Angel Jimenez de Cisneros Carreño <angeljcc@gmail.com>
2015-02-16 17:35:31 +0100

    Initial commit

---
+++ flaskEjemplo.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route('/hello')
+def hello_world():
+    return 'Hola gentuza!'
+
+@app.route('/')
+@app.route('/{name}')
+def hola(name = 'angel'):
+    return render_template('hello.html',name = name)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
