16acbc2b631a3a8e826ae5c8ef887d7a3b5fe632
Angel Jimenez de Cisneros Carreño <angeljcc@gmail.com>
2015-02-16 19:19:11 +0100

    #1 formularios

---
+++ config.py
@@ -0,0 +1,4 @@
+__author__ = 'root'
+
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'you-will-never-guess'
--- flaskEjemplo.py
+++ flaskEjemplo.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
+app.config.from_object('config')
 
+from flaskEjemplo import views
 
 @app.route('/hello')
 def hello_world():
